7f478a7c78d99bad8b4ca4185c2cd9f9957e251d
hann11galailaptrinh <email-cua-ban@gmail.com>
2025-12-24 15:30:37 +0700

    them run dev

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const PORT: number = 8080;
 
 
 app.get('/', (req, res)=>{
-    res.send("Hello World");
+    res.send("Hello World updated");
 })
 
 app.get('/hoidanit', (req, res)=>{
@@ -14,5 +14,5 @@ app.get('/hoidanit', (req, res)=>{
 })
 
 app.listen(8080,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 })
