7b8fb4b5e70833d12ba7d1f033b7d4349a728248
hann11galailaptrinh <email-cua-ban@gmail.com>
2025-12-24 15:09:47 +0700

    bai tiep

---
+++ app.ts
@@ -0,0 +1,18 @@
+// const express  = require('express');
+import express from 'express';
+
+const app = express();
+const PORT: number = 8080;
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+app.get('/hoidanit', (req, res)=>{
+    res.send("Hello Ha");
+})
+
+app.listen(8080,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
